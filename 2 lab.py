e9b758f10b7d2cbe8fe778f4b96c4229882be5d2
danchewgun <danya-nik08@yandex.ru>
2024-04-26 14:56:21 +0300

    2 lab

---
+++ 2 lab.py
@@ -0,0 +1,8 @@
+print("Введите пароль")
+first_password = input()
+print("Введите пароль повторно")
+second_password = input()
+if first_password == second_password:
+    print("Пароль принят")
+else:
+    print("Пароль не принят")
